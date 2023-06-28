5db77fc48c4f7ec3d271b96921457ff6676752ef
jcoop32 <jcooper11@hawk.iit.edu>
2023-06-27 22:05:33 -0500

    key switch removed

--- models/apiPrice.js
+++ models/apiPrice.js
@@ -4,32 +4,32 @@ module.exports = {
 
 //changes key after 90 calls
 
-let count = 0;
-key = process.env.STOCK_DATA_KEY6;
-switch (count) {
-  case 9:
-    key = process.env.STOCK_DATA_KEY6;
-    break;
-  case 18:
-    key = process.env.STOCK_DATA_KEY5;
-    break;
-  case 27:
-    key = process.env.STOCK_DATA_KEY4;
-    break;
-  case 36:
-    key = process.env.STOCK_DATA_KEY3;
-    break;
-  case 45:
-    key = process.env.STOCK_DATA_KEY2;
-    break;
-  case 54:
-    key = process.env.STOCK_DATA_KEY1;
-    break;
-  // default:
-  //   key = process.env.STOCK_DATA_KEY1;
-}
+// let count = 0;
+// key = process.env.STOCK_DATA_KEY6;
+// switch (count) {
+//   case 9:
+//     key = process.env.STOCK_DATA_KEY6;
+//     break;
+//   case 18:
+//     key = process.env.STOCK_DATA_KEY5;
+//     break;
+//   case 27:
+//     key = process.env.STOCK_DATA_KEY4;
+//     break;
+//   case 36:
+//     key = process.env.STOCK_DATA_KEY3;
+//     break;
+//   case 45:
+//     key = process.env.STOCK_DATA_KEY2;
+//     break;
+//   case 54:
+//     key = process.env.STOCK_DATA_KEY1;
+//     break;
+// default:
+//   key = process.env.STOCK_DATA_KEY1;
+//}
 
-console.log(key);
+// console.log(key);
 async function getData(stock) {
   console.log(' in getData');
 
@@ -40,12 +40,12 @@ async function getData(stock) {
   try {
     //https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
     const response = await fetch(
-      `https://api.stockdata.org/v1/data/quote?symbols=${stock}&api_token=${key}`,
+      `https://api.stockdata.org/v1/data/quote?symbols=${stock}&api_token=${process.env.STOCK_DATA_KEY1}`,
       requestOptions,
     );
 
-    count++;
-    console.log(count);
+    // count++;
+    // console.log(count);
     return response.json();
   } catch (error) {
     (error) => console.log('error', error);
