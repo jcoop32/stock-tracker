7f15ceaf0c3ce838fcb766823bd11e8b04e138c1
jcoop32 <jcooper11@hawk.iit.edu>
2023-11-26 17:41:02 -0600

    database config

--- config/database.js
+++ config/database.js
@@ -8,3 +8,20 @@ const db = mongoose.connection;
 db.on('connected', function () {
   console.log(`Connected to MongoDB ${db.name}, at ${db.host}:${db.port}`);
 });
+
+
+
+const MongoClient = require('mongodb').MongoClient;
+
+
+const client = new MongoClient(process.env.DATABASE_URL), { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Connect to the MongoDB instance
+client.connect((err) => {
+  if (err) {
+    console.error('Error connecting to MongoDB:', err);
+  } else {
+    console.log('Connected to MongoDB!');
+    // Your application logic here
+  }
+});
